c81035aa03c04468fc3fc9838317ced61a544641
limeng <limeng54231@163.com>
2023-08-03 16:26:14 +0800

    fix: 添加事件

--- page/component/pages/webview/webview.js
+++ page/component/pages/webview/webview.js
@@ -1,8 +1,20 @@
 Page({
   onShareAppMessage() {
     return {
-      title: 'webview',
-      path: 'page/component/pages/webview/webview',
-    }
+      title: "webview",
+      path: "page/component/pages/webview/webview",
+    };
   },
-})
+
+  message(e) {
+    console.log("e: ", e);
+  },
+
+  load(e) {
+    console.log("e: ", e);
+  },
+
+  error(e) {
+    console.log("e: ", e);
+  },
+});
