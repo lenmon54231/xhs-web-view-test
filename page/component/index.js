229071f687f944fc74ee6bf6f7ab1873e6c9fc73
limeng <limeng54231@163.com>
2023-08-03 16:21:09 +0800

    fix: 1

--- page/component/index.js
+++ page/component/index.js
@@ -11,27 +11,29 @@ Page({
   // },
   onShareAppMessage() {
     return {
-      title: '小程序官方组件展示',
-      path: 'page/component/index',
-    }
+      title: "小程序官方组件展示",
+      path: "page/component/index",
+    };
   },
 
   data: {
-    list: [{
-      id: 'open',
-      name: '开放能力',
-      open: false,
-      pages: ['webview'],
-    }, ],
+    list: [
+      {
+        id: "open",
+        name: "开放能力",
+        open: false,
+        pages: ["webview"],
+      },
+    ],
   },
   complete(res) {
-    console.log('=======complete:invoke', res)
+    console.log("=======complete:invoke", res);
   },
 
   reLaunch() {
     xhs.reLaunch({
-      url: '/page/component/pages/navigator/navigate?na=navigate',
-    })
+      url: "/page/component/pages/navigator/navigate?na=navigate",
+    });
   },
 
   // onLoad(query) {
@@ -43,18 +45,17 @@ Page({
   // },
 
   kindToggle(e) {
-    const id = e.currentTarget.id
-    const list = this.data.list
+    const id = e.currentTarget.id;
+    const list = this.data.list;
     for (let i = 0, len = list.length; i < len; ++i) {
       if (list[i].id === id) {
-        list[i].open = !list[i].open
+        list[i].open = !list[i].open;
       } else {
-        list[i].open = false
+        list[i].open = false;
       }
     }
     this.setData({
       list,
-    })
-    xhs?.reportAnalytics('click_view_programmatically', {})
+    });
   },
-})
+});
